23131a3a8275fd8c03af14c577b251feb2fba116
jerry73204 <jerry73204@gmail.com>
2015-07-20 07:35:06 +0800

    Move hardcoded parts to config package

--- bonbon/config/config.go
+++ bonbon/config/config.go
@@ -4,6 +4,17 @@ import (
 	"github.com/huandu/facebook"
 )
 
+// FBAppID App ID for Facebook graph api
 var FBAppID = "915780538494020"
+
+// FBAppSecret App Secret for Facebook graph api
 var FBAppSecret = "d7b698973175d799a27e0d129f9e19ba"
+
+// GlobalApp global app instance
 var GlobalApp = facebook.New(FBAppID, FBAppSecret)
+
+// DatabaseDriver driver string for gorm.Open()
+var DatabaseDriver = "sqlite3"
+
+// DatabaseArgs arguments for gorm.Open()
+var DatabaseArgs = "/tmp/bonbon.db"
--- bonbon/database/database.go
+++ bonbon/database/database.go
@@ -3,14 +3,21 @@ package database
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3" // provide sqlite3 driver
+	"bonbon/config"
 )
+
+var isDatabaseInitialized = false
 
 // GetDB start connection to database
 func GetDB() (gorm.DB) {
-	db, err := gorm.Open("sqlite3", "/tmp/bonbon.db")
+	db, err := gorm.Open(config.DatabaseDriver, config.DatabaseArgs)
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Account{}, &Friend{}, &ChatRoom{})
+
+	if !isDatabaseInitialized {
+		db.AutoMigrate(&Account{}, &Friend{}, &ChatRoom{})
+		isDatabaseInitialized = true
+	}
 	return db
 }
