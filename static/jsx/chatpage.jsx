e6bf8d00cb39b3b55cb12e7f8debd9cd2d57a05e
liu115 <jimmydiablo@gmail.com>
2016-05-10 20:10:04 +0800

    Add different color for bonbon button

--- static/jsx/chatpage.jsx
+++ static/jsx/chatpage.jsx
@@ -249,7 +249,15 @@ var ChatRoom = React.createClass({
       var node = React.findDOMNode(this.refs.refContent);
       node.scrollTop = this.scrollTop + (node.scrollHeight - this.scrollHeight);
     }.bind(this));
-	return {};
+		this.props.chatSocket.addHandler('bonbon', function(cmd) {
+			this.setState({bonboning: true});
+    }.bind(this));
+		this.props.chatSocket.addHandler('disconnect', function(cmd) {
+			this.setState({bonboning: false});
+    }.bind(this));
+	return {
+		bonboning: false
+		};
   },
   componentWillUpdate: function() {
     var node = React.findDOMNode(this.refs.refContent);
@@ -344,13 +352,20 @@ var ChatRoom = React.createClass({
           {function() {
             switch (this.props.target) {
               case 0:
-                return (
-                <div className="pull-left">
-                  <a id="button-bonbon" className="message-button" onClick={this.bonbon}>Bonbon!</a>
-                  <a id="button-report" className="message-button" onClick={this.disconnect}>離開</a>
-                </div>);
-              //default:
-                //return ();
+								switch (this.state.bonboning) {
+									case true:
+										return (
+											<div className="pull-left">
+			                  <a id="button-bonbon" className="message-button bonbon-button-clicked" onClick={this.bonbon}>Bonbon!</a>
+			                  <a id="button-report" className="message-button" onClick={this.disconnect}>離開</a>
+			                </div>);
+									case false:
+										return (
+											<div className="pull-left">
+			                  <a id="button-bonbon" className="message-button" onClick={this.bonbon}>Bonbon!</a>
+			                  <a id="button-report" className="message-button" onClick={this.disconnect}>離開</a>
+			                </div>);
+								}
             }
           }.bind(this)()}
           <div className="pull-right">
