83e6ab37c6db84c420519bb9faa5d9cf1307414d
MROS <yc1043@gmail.com>
2017-07-14 00:30:04 +0800

    砍去更多無用檔案、僅以go get管理套件

---
+++ install.sh
@@ -0,0 +1,9 @@
+go get github.com/jinzhu/gorm
+go get github.com/go-sql-driver/mysql
+go get github.com/mattn/go-sqlite3
+go get github.com/gin-gonic/gin
+go get github.com/gorilla/websocket
+go get github.com/huandu/facebook
+go get gopkg.in/gcfg.v1
+go get github.com/fatih/color
+
